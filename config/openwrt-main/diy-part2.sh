181d8ca619c7359f3ce9083c3e599b3662cfaef7
xiaobao1980 <47268315+xiaobao1980@users.noreply.github.com>
2024-09-12 17:07:46 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -24,11 +24,11 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 #
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
-#git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+rm -rf package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-git clone https://github.com/jerrykuku/luci-theme-argon.git
